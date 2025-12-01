6ed118c41afdd9aaf5207238945370853de16290
TC Teo <tsechin@tsechin.com>
2025-12-01 21:23:06 +1100

    generalize runner script

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+# usage: ./run.sh <impl> <day> [args...]
+
+impl="$1"
+shift || true
+day="$1"
+shift || true
+
+if [ -z "${impl}" ]; then
+    echo 'need impl'
+    exit 1
+fi
+if [ -z "${day}" ]; then
+    echo 'need day'
+    exit 1
+fi
+
+day="$(echo ${day} | awk -F '/' '{print $1}')"
+if [ ! -d "${day}" ]; then
+    echo "day ${day} not found"
+    exit 1
+fi
+if [ ! -d "${day}/${impl}" ]; then
+    echo "day ${day} implementation ${impl} not found"
+    exit 1u
+fi
+
+case "${impl}" in
+    'py')
+        uv run python -m "${day}.${impl}" "$@"
+        ;;
+    'go')
+        go run "${day}/${impl}/main.go" "$@"
+        ;;
+    *)
+        echo "don't know how to run impl ${impl}"
+        exit 1
+        ;;
+esac
