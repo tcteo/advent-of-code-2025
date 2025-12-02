24a8ee7eb878e670098dc5eb21e9c19fdcd6218a
TC Teo <tsechin@tsechin.com>
2025-12-02 18:49:33 +1100

    day 2 part 2

--- day02/py/__main__.py
+++ day02/py/__main__.py
@@ -1,5 +1,6 @@
 import os
 import re
+import functools
 from typing import Tuple
 
 _dir = os.path.dirname(__file__)
@@ -7,7 +8,7 @@ _dir = os.path.dirname(__file__)
 
 def read_input(filename):
   # returns a list of 2-tuples
-  inputfile = os.path.join(_dir, '../'+filename)
+  inputfile = os.path.join(_dir, '../' + filename)
   with open(inputfile) as f:
     input = f.read().strip()
     ranges = input.split(',')
@@ -16,30 +17,55 @@ def read_input(filename):
 
 
 re_invalid_id = re.compile(r'(\d+)\1$')
-def invalid_id(id: int) -> bool:
+
+
+def invalid_id_part1(id: int) -> bool:
   digits = str(id)
   if re_invalid_id.match(digits):
     return True
   return False
-  
 
 
-def day2part1(filename):
-  print('day 2 part 1: '+filename)
+@functools.cache
+def generate_regexes(n):
+  res = []
+  for i in range(1, n + 1):
+    re_str = r'(\d+)' + r'\1' * i + r'$'
+    res.append(re.compile(re_str))
+  return res
+
+
+def invalid_id_part2(id: int) -> bool:
+  digits = str(id)
+  regexes = generate_regexes(len(digits))
+  for r in regexes:
+    if r.match(digits):
+      return True
+  return False
+
+
+def day2(filename, invalid_id_fn):
+  print('day 2 part 1: ' + filename)
   ranges = read_input(filename)
   # print(ranges)
   invalid_id_sum = 0
   for start, end in ranges:
-    for id in range(int(start), int(end)+1):
-      if invalid_id(id):
-        # print(f'Invalid ID: {id}')
+    for id in range(int(start), int(end) + 1):
+      if invalid_id_fn(id):
+        # print(f'invalid id: {id}')
         invalid_id_sum += id
   print(f'Sum of invalid IDs: {invalid_id_sum}')
 
 
+def day2part1(filename):
+  return day2(filename, invalid_id_part1)
+
+
+def day2part2(filename):
+  return day2(filename, invalid_id_part2)
 
 
 if __name__ == '__main__':
-  day2part1('example.txt')
+  # day2part1('example.txt')
   day2part1('input.txt')
-  # day2part2()
+  day2part2('input.txt')
