00749afd84308500a98c0f221066d3d8ee85e869
TC Teo <tsechin@tsechin.com>
2025-12-03 19:40:37 +1100

    day 3 part 2

--- day03/py/__main__.py
+++ day03/py/__main__.py
@@ -2,6 +2,7 @@ import os
 import re
 import functools
 from typing import Tuple
+import numpy as np
 
 _dir = os.path.dirname(__file__)
 
@@ -46,8 +47,46 @@ def day3part1(filename):
     sum += bank_max_joltage
   print(f'{sum=}')
 
+def day3part2(filename):
+  sum = 0
+  inputlines = read_input(filename)
+  # print(inputlines)
+  for line in inputlines:
+    # print(line)
+    digits = [int(x) for x in list(line)]
+    maxvalue = day3part2_max(digits, 12)
+    # print(f'line {maxvalue=}')
+    sum += maxvalue
+  print(f'{sum=}')
+
+# def sum_digits(digits):
+#   tens = [10**n for n in range(0,len(digits))][::-1]
+#   return sum([x[0]*x[1] for x in zip(digits,tens)])
+
+def day3part2_max(digits, length):
+  # print(f'day3part2_max({digits}, {length}):')
+  if len(digits)<=0:
+    raise RuntimeError("too short")
+  if len(digits)<length:
+    raise RuntimeError("too short")
+  
+  range_max = len(digits)
+  while True:
+    max_idx = np.argmax(digits[:range_max])
+    max_val = digits[max_idx]
+    if length==1:
+      return max_val * (10**(length-1))
+    try:
+      r = day3part2_max(digits[max_idx+1:], length-1)
+      return max_val * (10**(length-1)) + r
+    except RuntimeError:
+      range_max = max_idx
+
+  # return sum_digits(digits) # TODO
+
 
 if __name__ == '__main__':
   # day3part1('example.txt')
-  day3part1('input.txt')
-  # day3part2('input.txt')
+  # day3part1('input.txt')
+  # day3part2('example.txt')
+  day3part2('input.txt')
