356471945868bf9d88927f41b0ade2dc65b58d18
TC Teo <tsechin@tsechin.com>
2025-12-08 20:44:09 +1100

    day 6 part 2

--- day06/py/__main__.py
+++ day06/py/__main__.py
@@ -37,8 +37,58 @@ def day6part1(filename):
   print(f'ans {np.sum(res)}')
   # print(res)
 
+def remove_newline(s):
+  if s.endswith('\n'):
+    return s[:-1]
+  return s
+
+def digits_to_int(dlist):
+  n = ''.join(dlist).strip()
+  if not n:
+    return None
+  return int(n)
+
+
+def read_input2(filename):
+  inputfile = os.path.join(_dir, '../' + filename)
+  with open(inputfile, 'rt') as f:
+    lines = f.readlines()
+    lines = [remove_newline(s) for s in lines]
+
+  m = [list(l) for l in lines]
+  mt = np.array(m).T
+  # return mt.tolist()
+  numbers = [digits_to_int(x) for x in mt[:,:-1]]
+  numbers.append(None)
+  operators = re.sub(r' +', ' ', ''.join(mt[:,-1]).strip()).split(' ')
+  # print('numbers:', numbers)
+  # print('operators:', operators)
+  return numbers, operators
+  
 def day6part2(filename):
   print(f'day 6 part 2: {filename}')
+  numbers, operators = read_input2(filename)
+  oi = 0
+  a = []
+  res = []
+  for i in numbers:
+    if i is None:
+      # do math
+      opchar = operators[oi]
+      fn, initvalue = opfns[opchar]
+      res.append(
+        functools.reduce(fn, a, initvalue)
+      )
+      # then clear
+      a = []
+      oi += 1
+    else:
+      # append
+      a.append(i)
+  # print(res)
+
+  print(f'ans {np.sum(res)}')
+  
 
 
 if __name__ == '__main__':
@@ -46,4 +96,4 @@ if __name__ == '__main__':
   day6part1('input.txt')
   # print('--')
   # day6part2('example.txt')
-  # day6part2('input.txt')
+  day6part2('input.txt')
